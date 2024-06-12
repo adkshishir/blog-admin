b85cbe77bff14bcf56879f739c97a2d0812618de
adkshishir <adhikarishishir50@gmail.com>
2024-06-12 23:47:44 +0545

    add : request helper

---
+++ src/helper/request.ts
@@ -0,0 +1,20 @@
+class Request {
+  async get(url: string) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+  async post(url: string, data: any) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    return result;
+  }
+}
+
+export default new Request();
