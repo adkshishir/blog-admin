f9cb11a08bd8576b69a7c808992ae93b4bf2c0fc
adkshishir <adhikarishishir50@gmail.com>
2024-06-12 23:47:33 +0545

    add : static end points

---
+++ src/constants/api.ts
@@ -0,0 +1,9 @@
+class Api {
+  public static readonly BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+  public static readonly API_URL = `${Api.BASE_URL}/api`;
+  public static readonly LOGIN_URL = `${Api.API_URL}/auth/login`;
+  public static readonly REGISTER_URL = `${Api.API_URL}/auth/register`;
+  public static readonly USERS_URL = `${Api.API_URL}/users`;
+}
+
+export default Api
