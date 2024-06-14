59fdaca8a5496445dd8c383f7db44d13cc72694d
adkshishir <adhikarishishir50@gmail.com>
2024-06-14 19:40:49 +0545

    add : post with media method to request

--- src/helper/request.ts
+++ src/helper/request.ts
@@ -1,19 +1,47 @@
 class Request {
   async get(url: string) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    const response = await fetch(url, {
+      cache: 'no-cache',
+    });
+    try {
+      const data = await response.json();
+      return data;
+    } catch {
+      return { status: 'error', message: 'Something went wrong' };
+    }
   }
-  async post(url: string, data: any) {
+  async post(url: string, data: any, token?: string) {
     const response = await fetch(url, {
       method: 'POST',
+      cache: 'no-cache',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(data),
     });
-    const result = await response.json();
-    return result;
+    try {
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      return { status: 'error', message: 'Something went wrong' };
+    }
+  }
+  async postWithMedia(url: string, data: any, token?: string) {
+    const response = await fetch(url, {
+      method: 'POST',
+      cache: 'no-cache',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: data,
+    });
+    try {
+      const result = await response.json();
+      return result;
+    } catch {
+      return { status: 'error', message: 'Something went wrong' };
+    }
   }
 }
 
