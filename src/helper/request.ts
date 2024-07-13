335a54a36919d1fc6f42c90487bca78c023a1ce8
adkshishir <adhikarishishir50@gmail.com>
2024-07-13 23:30:05 +0545

    update : request helper by adding patch function

--- src/helper/request.ts
+++ src/helper/request.ts
@@ -43,6 +43,55 @@ class Request {
       return { status: 'error', message: 'Something went wrong' };
     }
   }
+  async patch(url: string, data: any, token?: string) {
+    const response = await fetch(url, {
+      method: 'PATCH',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    });
+    try {
+      const result = await response.json();
+      return result;
+    } catch {
+      return { status: 'error', message: 'Something went wrong' };
+    }
+  }
+  async patchWithMedia(url: string, data: any, token?: string) {
+    const response = await fetch(url, {
+      method: 'PATCH',
+      cache: 'no-cache',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: data,
+    });
+    try {
+      const result = await response.json();
+      return result;
+    } catch {
+      return { status: 'error', message: 'Something went wrong' };
+    }
+  }
+  async delete(url: string, token?: string) {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    try {
+      const result = await response.json();
+      return result;
+    } catch {
+      return { status: 'error', message: 'Something went wrong' };
+    }
+  }
 }
 
 export default new Request();
