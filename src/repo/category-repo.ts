12f1107b96159702213960c5d8f7b70f05a5abed
adkshishir <adhikarishishir50@gmail.com>
2024-07-13 23:28:40 +0545

    update : category repo

--- src/repo/category-repo.ts
+++ src/repo/category-repo.ts
@@ -1,34 +1,57 @@
 import Api from '@/constants/api';
-import { getCookie } from '@/helper/cookiesHander';
+import { getCookie } from '@/helper/cookies-hander';
 import request from '@/helper/request';
 import toast from 'react-hot-toast';
 
 class CategoryRepo {
   async getAll(
-    e: FormData,
     success: (m: string, d: any) => void,
     failure: (d: any) => void
   ) {
     const response = await request.get(Api.CATEGORIES_URL);
-    const data = await response.json();
-    if (response.status.toString().startsWith('2')) {
+    if (response.status.toString().startsWith('success')) {
+      const data = await response.data
       success(response.message, data);
     } else {
       failure(response.message);
     }
-    return data;
+    // return data;
   }
   async create(
     e: FormData,
     success: (m: string, d: any) => void,
     failure: (d: any) => void
   ) {
-    'use server';
     const response = await request.postWithMedia(
       Api.CATEGORIES_URL,
       e,
       await getCookie('token')
     );
+    if (response.status == 'success') {
+      success(response.message, response.data);
+    } else {
+      failure(response.message);
+    }
+  }
+  async update(
+    id:number,
+    e: FormData,
+    success: (m: string, d: any) => void,
+    failure: (d: any) => void
+  ) {
+    const response = await request.patchWithMedia(
+      Api.CATEGORIES_URL + `/${id}`,
+      e,
+      await getCookie('token')
+    );
+    if (response.status == 'success') {
+      success(response.message, response.data);
+    } else {
+      failure(response.message);
+    }
+  }
+  async delete(id:number, success: (m: string, d: any) => void, failure: (d: any) => void) {
+    const response = await request.delete(Api.CATEGORIES_URL + `/${id}`, await getCookie('token'));
     if (response.status == 'success') {
       success(response.message, response.data);
     } else {
